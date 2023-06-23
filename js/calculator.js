1164e682551d099bce4608222869fe22ab37fe22
Yannic <yannic1992@gmx.net>
2023-06-23 16:44:18 +0200

    add bug-fix for calculation to work properly

--- js/calculator.js
+++ js/calculator.js
@@ -19,36 +19,39 @@ function divide(a, b){
 }
 
 function operate(){
-    //console.log(displayValue);
+    console.log(displayValue);
     inputArray = displayValue.split(/([+*/]|(?<=\d)-)/g);
-    //console.log(inputArray);
-    while(inputArray.length >1){
-        inputArray.filter(x => {
-            if(x == "*"){
-                inputArray[inputArray.indexOf(x)-1] = multiply(Number(inputArray[inputArray.indexOf(x)-1]), 
-                                                                Number(inputArray[inputArray.indexOf(x)+1]));
-                inputArray.splice(inputArray.indexOf(x), 2);
-                //console.log(inputArray);
-            }else if(x == "/"){
-                inputArray[inputArray.indexOf(x)-1] = divide(Number(inputArray[inputArray.indexOf(x)-1]), 
-                                                                Number(inputArray[inputArray.indexOf(x)+1]));
-                inputArray.splice(inputArray.indexOf(x), 2);
-                //console.log(inputArray);
+    console.log(inputArray);
+    while(inputArray.length > 1){
+        for(let x = 0; x <= inputArray.length; x++){
+            console.log("x = " + x + " ; Element: "+ inputArray[x]);
+            if(inputArray[x] == "*"){
+                inputArray[x-1] = multiply(Number(inputArray[x-1]), Number(inputArray[x+1]));
+                inputArray.splice(x, 2);
+                console.log(inputArray);
+                x -= 1;
+            }else if(inputArray[x] == "/"){
+                inputArray[x-1] = divide(Number(inputArray[x-1]), Number(inputArray[x+1]));
+                inputArray.splice(x, 2);
+                console.log(inputArray);
+                x -= 1;
             }
-        });
-        inputArray.filter(x => {
-            if(x == "+"){
-                inputArray[inputArray.indexOf(x)-1] = add(Number(inputArray[inputArray.indexOf(x)-1]), 
-                                                                Number(inputArray[inputArray.indexOf(x)+1]));
-                inputArray.splice(inputArray.indexOf(x), 2);
-                //console.log(inputArray);
-            }else if(x == "-"){
-                inputArray[inputArray.indexOf(x)-1] = subtract(Number(inputArray[inputArray.indexOf(x)-1]), 
-                                                                Number(inputArray[inputArray.indexOf(x)+1]));
-                inputArray.splice(inputArray.indexOf(x), 2);
-                //console.log(inputArray);
+        }
+
+        for(let x = 0; x <= inputArray.length; x++){
+            console.log("x = " + x + " ; Element: "+ inputArray[x]);
+            if(inputArray[x] == "+"){
+                inputArray[x-1] = add(Number(inputArray[x-1]), Number(inputArray[x+1]));
+                inputArray.splice(x, 2);
+                console.log(inputArray);
+                x -= 1;
+            }else if(inputArray[x] == "-"){
+                inputArray[x-1] = subtract(Number(inputArray[x-1]), Number(inputArray[x+1]));
+                inputArray.splice(x, 2);
+                console.log(inputArray);
+                x -= 1;
             }
-        });
+        }
     }
     document.querySelector(".display").textContent = displayValue + "=" + inputArray[0];
 }
