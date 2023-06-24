24ad88411d49020eb67299bdb1ca6c31cb5e19ec
Yannic <yannic1992@gmx.net>
2023-06-24 16:49:09 +0200

    add line break for result and prevent calculation with final math symbol

--- js/calculator.js
+++ js/calculator.js
@@ -20,43 +20,45 @@ function divide(a, b){
 }
 
 function operate(){
-    //console.log(displayValue);
-    inputArray = displayValue.split(/([+*/]|(?<=\d)-)/g);
-    //console.log(inputArray);
-    while(inputArray.length > 1){
-        for(let x = 0; x <= inputArray.length; x++){
-            //console.log("x = " + x + " ; Element: "+ inputArray[x]);
-            if(inputArray[x] == "*"){
-                inputArray[x-1] = multiply(Number(inputArray[x-1]), Number(inputArray[x+1]));
-                inputArray.splice(x, 2);
-                //console.log(inputArray);
-                x -= 1;
-            }else if(inputArray[x] == "/"){
-                inputArray[x-1] = divide(Number(inputArray[x-1]), Number(inputArray[x+1]));
-                inputArray.splice(x, 2);
-                //console.log(inputArray);
-                x -= 1;
+    if(!isNaN(displayValue.charAt(displayValue.length-1))) {
+        //console.log(displayValue);
+        inputArray = displayValue.split(/([+*/]|(?<=\d)-)/g);
+        //console.log(inputArray);
+        while(inputArray.length > 1){
+            for(let x = 0; x <= inputArray.length; x++){
+                //console.log("x = " + x + " ; Element: "+ inputArray[x]);
+                if(inputArray[x] == "*"){
+                    inputArray[x-1] = multiply(Number(inputArray[x-1]), Number(inputArray[x+1]));
+                    inputArray.splice(x, 2);
+                    //console.log(inputArray);
+                    x -= 1;
+                }else if(inputArray[x] == "/"){
+                    inputArray[x-1] = divide(Number(inputArray[x-1]), Number(inputArray[x+1]));
+                    inputArray.splice(x, 2);
+                    //console.log(inputArray);
+                    x -= 1;
+                }
+            }
+            for(let x = 0; x <= inputArray.length; x++){
+                //console.log("x = " + x + " ; Element: "+ inputArray[x]);
+                if(inputArray[x] == "+"){
+                    inputArray[x-1] = add(Number(inputArray[x-1]), Number(inputArray[x+1]));
+                    inputArray.splice(x, 2);
+                    //console.log(inputArray);
+                    x -= 1;
+                }else if(inputArray[x] == "-"){
+                    inputArray[x-1] = subtract(Number(inputArray[x-1]), Number(inputArray[x+1]));
+                    inputArray.splice(x, 2);
+                    //console.log(inputArray);
+                    x -= 1;
+                }
             }
         }
-        for(let x = 0; x <= inputArray.length; x++){
-            //console.log("x = " + x + " ; Element: "+ inputArray[x]);
-            if(inputArray[x] == "+"){
-                inputArray[x-1] = add(Number(inputArray[x-1]), Number(inputArray[x+1]));
-                inputArray.splice(x, 2);
-                //console.log(inputArray);
-                x -= 1;
-            }else if(inputArray[x] == "-"){
-                inputArray[x-1] = subtract(Number(inputArray[x-1]), Number(inputArray[x+1]));
-                inputArray.splice(x, 2);
-                //console.log(inputArray);
-                x -= 1;
-            }
-        }
+        document.querySelector(".display").innerText = displayValue + "\n=" + inputArray[0];
+        displayValue = inputArray[0];
+        calculationDone = true;
+        //console.log(displayValue);
     }
-    document.querySelector(".display").textContent = displayValue + "=" + inputArray[0];
-    displayValue = inputArray[0];
-    calculationDone = true;
-    //console.log(displayValue);
 }
 
 function clickInput(input){
